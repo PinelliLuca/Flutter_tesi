c33644482da71a1a184e3ca19e7572ffc70c7952
PinelliLuca <p.lucapinelli@gmail.com>
2023-10-27 23:49:58 +0200

    non serve questa page

--- lib/Pages/ShoppingCart.dart
+++ lib/Pages/ShoppingCart.dart
@@ -4,7 +4,16 @@ class ShoppingCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Contenuto del carrello della spesa
+      appBar: AppBar(
+        title: Text('Shopping Cart'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Container(
+    child:  Image( image: AssetImage('assets/Gif/busta.gif'),
+    ),
+      ),
+
+
     );
   }
 }
