d7f497f0eec2f9a016f9e44458b0caa2edd640a6
PinelliLuca <p.lucapinelli@gmail.com>
2023-10-28 16:22:19 +0200

    Sport e dressRoom con immagini per sport

---
+++ lib/Pages/Sport.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Sport extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sport'),
+        backgroundColor: Colors.lightBlueAccent[900],
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 300,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/runners.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            top: -20,
+            left: -120,  // Mancava il due punti
+            child: Container(
+              width: 400,
+              height: 400,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/womanrun-01.png'),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Sport",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Abbigliamento sportivo",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
