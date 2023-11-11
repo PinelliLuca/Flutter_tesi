52d8a257631cb801fb307eead0c14ea208d696e7
PinelliLuca <p.lucapinelli@gmail.com>
2023-11-11 22:23:36 +0100

    DressRoom fatta. Bozza, da migliorare esteticamente

--- lib/Pages/DressRoom.dart
+++ lib/Pages/DressRoom.dart
@@ -5,60 +5,47 @@ class DressRoom extends StatefulWidget {
   _DressRoomState createState() => _DressRoomState();
 }
 
-
 class _DressRoomState extends State<DressRoom> {
-  String currentMaglia = "";
-  String currentPantalone = "";
+  List<String> maglie = [
+    "assets/images/maglia1.png",
+    "assets/images/maglia2.jpeg",
+    "assets/images/maglia1.png",
+    // Aggiungi altre immagini se necessario
+  ];
 
-  List<String> maglie = [];
-  List<String> pantaloni = [];
+  List<String> pantaloni = [
+    "assets/images/pantalone1-removebg.png",
+    "assets/images/pantalone1.jpg",
+    "assets/images/pantalone1-removebg.png",
+    // Aggiungi altre immagini se necessario
+  ];
 
   int indexMaglia = 0;
   int indexPantalone = 0;
 
-  @override
-  void initState() {
-    super.initState();
-
-    maglie = [
-      "assets/images/camerino/maglia1.jpeg",
-      "assets/images/camerino/maglia2.jpeg",
-      "assets/images/camerino/maglia3.jpeg",
-    ];
-
-    pantaloni = [
-      "assets/images/camerino/pantalone1.jpeg",
-      "assets/images/camerino/pantalone2.jpeg",
-      "assets/images/camerino/pantalone3.jpeg",
-    ];
-
-    // Imposta la maglia e il pantalone iniziali
-    currentMaglia = maglie[indexMaglia];
-    currentPantalone = pantaloni[indexPantalone];
-  }
-
   void nextMaglia() {
-    indexMaglia = (indexMaglia + 1) % maglie.length;
-    currentMaglia = maglie[indexMaglia];
-    setState(() {});
+    setState(() {
+      indexMaglia = (indexMaglia + 1) % maglie.length;
+    });
   }
 
   void previousMaglia() {
-    indexMaglia = (indexMaglia - 1 + maglie.length) % maglie.length;
-    currentMaglia = maglie[indexMaglia];
-    setState(() {});
+    setState(() {
+      indexMaglia = (indexMaglia - 1 + maglie.length) % maglie.length;
+    });
   }
 
   void nextPantalone() {
-    indexPantalone = (indexPantalone + 1) % pantaloni.length;
-    currentPantalone = pantaloni[indexPantalone];
-    setState(() {});
+    setState(() {
+      indexPantalone = (indexPantalone + 1) % pantaloni.length;
+    });
   }
 
   void previousPantalone() {
-    indexPantalone = (indexPantalone - 1 + pantaloni.length) % pantaloni.length;
-    currentPantalone = pantaloni[indexPantalone];
-    setState(() {});
+    setState(() {
+      indexPantalone =
+          (indexPantalone - 1 + pantaloni.length) % pantaloni.length;
+    });
   }
 
   @override
@@ -70,53 +57,55 @@ class _DressRoomState extends State<DressRoom> {
       ),
       body: Column(
         children: [
-          // ScrollView delle maglie
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: GestureDetector(
-              onHorizontalDragEnd: (details) {
-                if (details.primaryVelocity! > 0) {
-                  nextMaglia();
-                } else if (details.primaryVelocity! < 0) {
-                  previousMaglia();
-                }
-              },
-              child: Row(
-                children: [
-                  for (int i = 0; i < maglie.length; i++)
-                    Image.asset(
-                      maglie[i],
-                      width: 100,
-                    ),
-                ],
-              ),
+          // Scritta Superiore
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Scegli le tue combinazioni',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-          // ScrollView dei pantaloni
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: GestureDetector(
-              onHorizontalDragEnd: (details) {
-                if (details.primaryVelocity! > 0) {
-                  nextPantalone();
-                } else if (details.primaryVelocity! < 0) {
-                  previousPantalone();
-                }
-              },
-              child: Row(
-                children: [
-                  for (int i = 0; i < pantaloni.length; i++)
-                    Image.asset(
-                      pantaloni[i],
-                      width: 100,
-                    ),
-                ],
-              ),
+          // Sezione Maglie
+          Expanded(
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: previousMaglia,
+                  icon: Icon(Icons.arrow_back),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    maglie[indexMaglia],
+                    width: double.infinity,
+                  ),
+                ),
+                IconButton(
+                  onPressed: nextMaglia,
+                  icon: Icon(Icons.arrow_forward),
+                ),
+              ],
             ),
           ),
-          // Immagine del vestito attualmente selezionato
-          Image.asset(
-            "$currentMaglia-$currentPantalone",
+          // Sezione Pantaloni
+          Expanded(
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: previousPantalone,
+                  icon: Icon(Icons.arrow_back),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    pantaloni[indexPantalone],
+                    width: double.infinity,
+                  ),
+                ),
+                IconButton(
+                  onPressed: nextPantalone,
+                  icon: Icon(Icons.arrow_forward),
+                ),
+              ],
+            ),
           ),
         ],
       ),
