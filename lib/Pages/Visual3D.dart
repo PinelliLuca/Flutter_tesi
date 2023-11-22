9583d7753ac3740a7cdf65fcf04b98d04ce96d3f
PinelliLuca <p.lucapinelli@gmail.com>
2023-11-22 14:52:57 +0100

    modifica appbar visual3d

--- lib/Pages/Visual3D.dart
+++ lib/Pages/Visual3D.dart
@@ -3,23 +3,28 @@ import 'package:o3d/o3d.dart';
 
 class Visual3D extends StatelessWidget {
   O3DController controller = O3DController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("O3D"),
-            backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: const Icon(Icons.arrow_back)),
+          title: Text("O3D"),
+          backgroundColor: Colors.blueGrey[900],
           actions: [
             IconButton(
-              onPressed: () =>
-              controller.cameraOrbit(20, 20, 5),
-              icon:const Icon(Icons.change_circle)),
+              onPressed: () => controller.cameraOrbit(20, 20, 5),
+              icon: const Icon(Icons.change_circle),
+            ),
             IconButton(
-            onPressed: () =>
-            controller.cameraTarget(1.2,1,4),
-    icon:const Icon(Icons.change_circle_outlined)),
-          ],
+              onPressed: () => controller.cameraTarget(1.2, 1, 4),
+              icon: const Icon(Icons.change_circle_outlined),
+            ),
 
+          ],
         ),
         body: O3D(
           controller: controller,
